8fa7c01986ddc99a288c78bccd944beff16c0b22
DmitryShvets <dima.shvets95@inbox.ru>
2021-10-30 14:40:44 +0300

    new library function DaysInYear;

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 // Created by Dmitry on 30.10.2021.
 //
 
-int main(){
+int DaysInYear(int year);
 
+int DaysInYear(int year) {
+    if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))return 366;
+    else return 365;
+}
+
+int main() {
 
     return 0;
 };
